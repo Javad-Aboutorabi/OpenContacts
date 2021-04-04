e4c0d7081f3d8523cb54f6f9bf867e56279b4453
Sultan ahamar <sultanm@sultanm.local>
2021-04-04 11:47:45 +0530

    Caller id now has a close icon to remove it

--- app/src/main/java/opencontacts/open/com/opencontacts/broadcast_recievers/PhoneStateReceiver.java
+++ app/src/main/java/opencontacts/open/com/opencontacts/broadcast_recievers/PhoneStateReceiver.java
@@ -112,6 +112,8 @@ public class PhoneStateReceiver extends BroadcastReceiver {
         LayoutInflater layoutinflater = LayoutInflater.from(context);
         drawOverIncomingCallLayout = layoutinflater.inflate(R.layout.draw_over_incoming_call, null);
         TextView contactName = drawOverIncomingCallLayout.findViewById(R.id.name_of_contact);
+        View close = drawOverIncomingCallLayout.findViewById(R.id.close);
+        close.setOnClickListener(v -> removeCallerIdDrawing(context));
         contactName.setText(context.getString(R.string.caller_id_text, callingContact.getFullName()));
         int typeOfWindow;
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
